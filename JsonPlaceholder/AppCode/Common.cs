4372f0ea5c43730a0e341fd76900cba9a11e272b
Mattosaurus <mattosaurus@outlook.com>
2019-03-10 13:58:59 +0000

    Get album

--- JsonPlaceholder/AppCode/Common.cs
+++ JsonPlaceholder/AppCode/Common.cs
@@ -40,11 +40,31 @@ namespace JsonPlaceholder.AppCode
             return albums;
         }
 
-        public static Album GetAlbum(int id)
+        public static async Task<Album> GetAlbum(int id)
         {
             Album album = new Album();
 
+            HttpClient client = new HttpClient();
 
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(String.Format("http://jsonplaceholder.typicode.com/albums/", id.ToString()));
+
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    string message = String.Format("GET failed. Received HTTP {0}", response.StatusCode);
+                    throw new ApplicationException(message);
+                }
+
+                byte[] responseByteArray = await response.Content.ReadAsByteArrayAsync();
+                string responseString = Encoding.UTF8.GetString(responseByteArray, 0, responseByteArray.Length);
+
+                album = JsonConvert.DeserializeObject<Album>(responseString);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
             return album;
         }
