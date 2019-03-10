4957cb401167b14075ed74c70504922b92d81a3d
Mattosaurus <mattosaurus@outlook.com>
2019-03-10 14:39:35 +0000

    Show photo details

---
+++ JsonPlaceholder/View Components/PhotoDetailComponent.cs
@@ -0,0 +1,17 @@
+﻿using JsonPlaceholder.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsonPlaceholder.View_Components
+{
+    public class PhotoDetailComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Album album)
+        {
+            return View(album);
+        }
+    }
+}
